1a457828a4fefad17b08e42c4709c113e3fecb23
Armando Rojas <armando.develop@gmail.com>
2022-06-23 16:57:55 -0400

    change

--- src/services/peopleService.js
+++ src/services/peopleService.js
@@ -1,6 +1,8 @@
 db = require("../models");
 
 exports.upsertPeople = async (data) => {
+  console.log(data);
+  process.exit(0);
   const People = await db.people.findOne({ where: { id: data.id } });
 
   if (People) return await db.people.update(data, { where: { id: data.id } });
