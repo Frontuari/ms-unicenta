eef73a229e3a7dbe9b6bc991c14d04a2c9dd217b
Armando Rojas <armando.develop@gmail.com>
2022-06-23 16:59:02 -0400

    change password

--- src/services/peopleService.js
+++ src/services/peopleService.js
@@ -2,7 +2,6 @@ db = require("../models");
 
 exports.upsertPeople = async (data) => {
   console.log(data);
-  process.exit(0);
   const People = await db.people.findOne({ where: { id: data.id } });
 
   if (People) return await db.people.update(data, { where: { id: data.id } });
